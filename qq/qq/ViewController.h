5e88bd771c7bb1cb8befba11f9bde8ce0487c78c
congxiaobaicaikaishi <ios-39@ACC80849.ipt.aol.com>
2015-10-30 11:13:55 +0800

    banben1
    hahahahahahawhhahsahahahahahahahaha

---
+++ qq/qq/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  qq
+//
+//  Created by ios－39 on 15/10/30.
+//  Copyright © 2015年 ios－39. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ qq/qq/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  qq
+//
+//  Created by ios－39 on 15/10/30.
+//  Copyright © 2015年 ios－39. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
